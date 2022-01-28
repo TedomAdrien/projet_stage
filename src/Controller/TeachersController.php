2af50f66f9c284673ffe59f0b069084b4db16a60
Tedom_Adrien <tedomadrein451@gmail.com>
2022-01-28 14:38:33 +0100

    ajoute du formulaire

--- src/Controller/TeachersController.php
+++ src/Controller/TeachersController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\Teacher;
 use App\Repository\TeacherRepository;
 use phpDocumentor\Reflection\DocBlock\Tag;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,7 +22,27 @@ class TeachersController extends AbstractController
     }
 
     /**
-     * @Route("/teachers/teacher/{id<(0-9)+>}", name="teachers_teacher_show")
+     * @Route("/teachers/create", name="/create")
+     */
+    public function create() : Response
+    {
+        $form = $this->createFormBuilder()
+        ->add('title')
+        ->add('description')
+        ->getForm()
+        ;
+
+        return $this->render('teachers/create.html.twig', [
+            'form'=> $form ->createView()
+
+        ]);
+
+    }
+
+
+
+    /**
+     * @Route("/teachers/show/{id<(0-9)+>}", name="/show")
      */
     public function show(Teacher $teacher) : Response 
     {
