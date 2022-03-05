6ead8944ea427f402aa436decdbb5eb2aad3c414
Tedom_Adrien <tedomadrein451@gmail.com>
2022-03-05 12:26:08 +0100

    rien de donne

--- src/Controller/PresentController.php
+++ src/Controller/PresentController.php
@@ -2,19 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Teacher;
+use App\Entity\Presence;
+use App\Form\PrensentType;
 use App\Repository\TeacherRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PresentController extends AbstractController
 {
     /**
-     * @Route("/present", name="present")
+     * @Route("/present", name="present_choice")
      */
     public function index(TeacherRepository $teacherRepository): Response
     {
         $teachers = $teacherRepository->findAll();
-        return $this->render('present/index.html.twig', compact('teachers'));
+        return $this->render('present/choice.html.twig', compact('teachers'));
     }
+
+    /**
+     * @Route("/present/create", name="present_create")
+     */
+    public function create(): Response
+    {
+
+        return $this->render('present/fiche.html.twig');
+    }
+
+       
 }
